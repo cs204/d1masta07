182cd981c12eec6ed2ea0a97b41a689669bbf49c
test <test>
2023-10-18 01:11:57 +0700

    test

---
+++ calories.py
@@ -0,0 +1,27 @@
+CALORIES_TABLE = {
+    "Apple": 130,
+    "Avocado": 50,
+    "Banana": 110,
+    "Cantaloype": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lime": 20,
+    "Nectarine": 60,
+    "Orange": 80,
+    "Peach": 60,
+    "Pear": 100,
+    "Pineapple": 50,
+    "Plums": 70,
+    "Strawberries": 50,
+    "Sweet Cherries": 100,
+    "Tangerine": 50,
+    "Watermelon": 80,
+}
+
+
+user_input = input("Фрукт: ")
+calories = CALORIES_TABLE.get(user_input, -1)
+print(f"Калории: {calories}")
